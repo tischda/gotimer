301e1d095d0882896e2a0ecd9a32411d63bdcffd
Daniel TISCHER <dos.7182@gmail.com>
2015-05-14 15:20:12 +0200

    reformat

--- main.go
+++ main.go
@@ -1,31 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "syscall"
-    "unsafe"
+	"fmt"
+	"log"
+	"syscall"
+	"unsafe"
 )
 
 const path = `SOFTWARE\Microsoft\Windows NT\CurrentVersion`
 const name = `ProductId`
 
 func main() {
-    var handle syscall.Handle
+	var handle syscall.Handle
 
-    err := syscall.RegOpenKeyEx(syscall.HKEY_LOCAL_MACHINE, syscall.StringToUTF16Ptr(path), 0, syscall.KEY_READ, &handle)
+	err := syscall.RegOpenKeyEx(syscall.HKEY_LOCAL_MACHINE, syscall.StringToUTF16Ptr(path), 0, syscall.KEY_READ, &handle)
 	if err != nil {
 		log.Fatal(err)
 	}
-    defer syscall.RegCloseKey(handle)
+	defer syscall.RegCloseKey(handle)
 
-    var buf[syscall.MAX_LONG_PATH]uint16
-    n := uint32(len(buf))
+	var buf [syscall.MAX_LONG_PATH]uint16
+	var typ uint32
+	n := uint32(len(buf) * 2) // api expects array of bytes, not uint16
 
-    err = syscall.RegQueryValueEx(handle, syscall.StringToUTF16Ptr(name), nil, nil, (*byte)(unsafe.Pointer(&buf[0])), &n)
-    if err != nil {
-        return
-    }
+	err = syscall.RegQueryValueEx(
+		handle, syscall.StringToUTF16Ptr(name),
+		nil,
+		&typ,
+		(*byte)(unsafe.Pointer(&buf[0])),
+		&n)
 
-    fmt.Printf("%s=%q\n", name, syscall.UTF16ToString(buf[:]))
+	if err != nil {
+        log.Fatal(err)
+	}
+	if typ != syscall.REG_SZ {
+        log.Fatalln("Expected key of type REG_SZ, but was: ", typ)
+	}
+
+	fmt.Printf("%s=%q\n", name, syscall.UTF16ToString(buf[:]))
 }
