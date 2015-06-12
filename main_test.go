03bfef36a1cbdaedbdca3ef6d651b10f27fe71f3
Daniel TISCHER <dos.7182@gmail.com>
2015-06-12 16:22:12 +0200

    fix lag in registry setting/reading fails build

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -11,12 +12,12 @@ func TestMainTimer(t *testing.T) {
 	args := []string{"-start", "tmain", "-stop", "tmain"}
 	os.Args = append(os.Args, args...)
 
-	expected := "Elapsed time (tmain): 0\n"
+	expected := "Elapsed time (tmain):"
 
 	// this can be done only once or test framework will panic
 	actual := captureOutput(main)
 
-	if expected != actual {
+	if !strings.Contains(actual, expected) {
 		t.Errorf("Expected: %s, but was: %s", expected, actual)
 	}
 }
