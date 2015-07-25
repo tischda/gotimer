bba1cba71f3f83e77de614c82615c84defca8927
Daniel TISCHER <dos.7182@gmail.com>
2015-07-25 19:57:18 +0200

    added build scripts

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"text/template"
 )
 
-const version string = "1.2.0"
+// http://technosophos.com/2014/06/11/compile-time-string-in-go.html
+// go build -ldflags "-x main.version $(git describe --tags)"
+var version string
 
 // command line flags
 var showVersion bool
